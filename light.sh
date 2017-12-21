ff329eae181e40f7f778c8c708c5ae40eed452ab
Bruce Jones <bruce.david.jones@gmail.com>
2017-12-21 16:27:09 -0500

    Update light.sh

--- light.sh
+++ light.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
- while :
- do
-	echo "Press [CTRL+C] to stop.."
-	sleep 4
-done
-#echo "hello4"
-#return 0
+# while :
+# do
+#	echo "Press [CTRL+C] to stop.."
+#	sleep 4
+#done
+echo "hello4"
+return 0
